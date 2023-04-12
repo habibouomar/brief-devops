a99b5095b2fe5f14e501ed5c3fa2fdd8b6284f52
omha <omar.habibou@octo.com>
2023-04-12 11:00:22 +0200

    add template engine

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import express from "express";
+import path from "path"
 
+const __dirname = path.resolve();
 const app = express();
+
+app.set("view engine", "ejs")
+app.set("views", "views")
+
+app.use(express.static(path.join(__dirname, "public")))
 
 app.get("/", (req, res) => {
     res.send("Hello World Omar !")
